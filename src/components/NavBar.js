853d16da49365376621ac36dbd05110404099000
misskip <anita.desanmiguel@gmail.com>
2021-12-26 18:22:24 +1100

    Completed Asignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,19 @@ import React from "react";
 
 function NavBar() {
   // update the JSX being returned!
-  return <nav>NavBar</nav>;
-}
+  return (
+  <nav>
+  <a
+    className="Home"
+    href="https://src/components/Home.js"
+    target="_blank"
+    >Home</a>
+  <a
+    className="About"
+    href="https://src/components/About.js"
+    target="_blank"
+    >About</a>
+  </nav>
+  )}
 
 export default NavBar;
